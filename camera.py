193e7885069416c05a184f125c0d796a034037b1
Shubhampadale <spadale0999@gmail.com>
2021-03-03 22:50:24 +0530

    Initial commit

--- camera.py
+++ camera.py
@@ -1,21 +1,36 @@
 import cv2
-face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+# defining face detector
+face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+smile_cascade = cv2.CascadeClassifier("haarcascade_smile.xml")
 ds_factor=0.6
-
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture(0)
+       #capturing video
+       self.video = cv2.VideoCapture(0)
     
     def __del__(self):
+        #releasing camera
         self.video.release()
-    
     def get_frame(self):
-        success, image = self.video.read()
-        image=cv2.resize(image,None,fx=ds_factor,fy=ds_factor,interpolation=cv2.INTER_AREA)
-        gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+       #extracting frames
+        ret, frame = self.video.read()
+        frame=cv2.resize(frame,None,fx=ds_factor,fy=ds_factor,
+        interpolation=cv2.INTER_AREA)                    
+        gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         face_rects=face_cascade.detectMultiScale(gray,1.3,5)
         for (x,y,w,h) in face_rects:
-        	cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
-        	break
-        ret, jpeg = cv2.imencode('.jpg', image)
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            smiles = smile_cascade.detectMultiScale(roi_gray,
+                scaleFactor = 1.5, minNeighbors = 15, minSize = (25,25))
+            for i in smiles:
+                if len(smiles)>1 :
+                    cv2.putText(frame,"Smiling",(30,30),
+                    cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),3,
+                    cv2.LINE_AA)
+                img_name = "haar_selfie.png"
+                cv2.imwrite(img_name,frame)   
+            break
+        # encode OpenCV raw frame to jpg and displaying it
+        ret, jpeg = cv2.imencode('.jpg', frame)
         return jpeg.tobytes()
